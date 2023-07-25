87beaa19cf4537fa0a4994b8f8ccc3328680e9c0
DaNny <danniskolobosiky@gmail.com>
2023-07-25 23:15:11 +0100

    added the api to suggest meals for users

--- src/api/v1/controllers/mealSuggestion.ts
+++ src/api/v1/controllers/mealSuggestion.ts
@@ -3,10 +3,12 @@ import axios from "axios";
 import config from "../../config";
 import { successResponse, handleError, errorResponse } from "../utils/responses";
 import {userMealOptions} from "../dummyDb/mealOptions";
+import { shuffleArray } from "../helpers/helper";
 
 
 const suggestUserMeals = async (req: Request, res: Response) => {
   try {
+    // let responseData = shuffleArray(userMealOptions);
     
     return successResponse(res, 200, "Meal options fetched succesfully", userMealOptions);
   } catch (error) {
